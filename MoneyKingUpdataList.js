17480ef1d2a75a05741572f7711639a019f5e38a
64071181 <144258416+64071181@users.noreply.github.com>
2024-11-13 09:51:08 +0800

    Update MoneyKingUpdataList.js

--- MoneyKingUpdataList.js
+++ MoneyKingUpdataList.js
@@ -1,1 +1,1 @@
-MoneyKingUpdataList = "https://mega.co.nz/#!0Fw1QDyC!3u9dyKOdJJaheoLOciXcDcZCxRpa8oVHK8ZiOVrwD4k","2","3","4"]
+MoneyKingUpdataList = ["https://mega.co.nz/#!0Fw1QDyC!3u9dyKOdJJaheoLOciXcDcZCxRpa8oVHK8ZiOVrwD4k","2","3","4"]
